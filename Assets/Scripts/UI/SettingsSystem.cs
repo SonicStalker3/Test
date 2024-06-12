2a8052de30ed0512f46106f18f7cda8823210579
SonicStalker3 <52555864+SonicStalker3@users.noreply.github.com>
2024-06-12 09:45:51 +0800

    InputSys.cs is changed to improved structure

--- Assets/Scripts/UI/SettingsSystem.cs
+++ Assets/Scripts/UI/SettingsSystem.cs
@@ -3,19 +3,8 @@ using UnityEngine;
 public class SettingsSystem : MonoBehaviour
 {
     public GameObject backBtn;
-    
-    void Awake()
-    {
-        
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    void GeneralBtn()
     {
         
     }
